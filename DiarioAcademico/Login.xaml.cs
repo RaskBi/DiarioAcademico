f69454f8e29da8248daabd298fe0edfaa7ab5688
Paul Blacio <sfq_pb@hotmail.com>
2021-11-22 15:16:14 -0500

    Agregar archivos de proyecto.

---
+++ DiarioAcademico/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Firebase.Auth;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DiarioAcademico
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public string WebAPIkey = "AIzaSyBJpv7lEK_IuBI5-AzTnoiQgbwf4yL3u3U";
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WebAPIkey));
+            try
+            {
+                var auth = await authProvider.SignInWithEmailAndPasswordAsync(txtEmail.Text, txtPass.Text);
+                var content = await auth.GetFreshAuthAsync();
+                var serializedcontnet = JsonConvert.SerializeObject(content);
+                Preferences.Set("MyFirebaseRefreshToken", serializedcontnet);
+                //await Navigation.PushAsync(new MyDashboardPage());
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", "Invalid useremail or password", "OK");
+            }
+        }
+    }
+}
---
+++ DiarioAcademico/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Firebase.Auth;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace DiarioAcademico
+{
+    public partial class MainPage : ContentPage
+    {
+        public string WebAPIkey = "AIzaSyBJpv7lEK_IuBI5-AzTnoiQgbwf4yL3u3U";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WebAPIkey));
+                var auth = await authProvider.CreateUserWithEmailAndPasswordAsync(txtNEmail.Text, txtNcontra.Text);
+                string gettoken = auth.FirebaseToken;
+                await App.Current.MainPage.DisplayAlert("Alert", gettoken, "Ok");
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "OK");
+            }
+        }
+        private async void btnIrLog_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+    }
+}
